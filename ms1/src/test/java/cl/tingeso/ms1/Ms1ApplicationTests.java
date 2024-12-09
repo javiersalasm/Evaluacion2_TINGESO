a80eaa3ceb363cf49f9c30bd6c92ef7c8402a184
javiersalasm <javier.salas.m@usach.cl>
2024-12-09 15:06:17 -0300

    Se sube archivos base para ms1 y ms2

---
+++ ms1/src/test/java/cl/tingeso/ms1/Ms1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.tingeso.ms1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
